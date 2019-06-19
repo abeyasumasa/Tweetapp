d68e1c44b16ba8797fe4a863c5d7f9b8186d1db8
yasumasa abe <abeyasumasa@MacBook-Air.local>
2019-06-19 23:24:24 +0900

    インデントを修正

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -3,57 +3,56 @@ class TweetsController < ApplicationController
 
    def index
      @tweets = Tweet.all
+    end
+
+    def new
+      if params[:back]
+        @tweet = Tweet.new(tweet_params)
+      else
+        @tweet = Tweet.new
+      end
    end
 
-   def new
-     if params[:back]
-       @tweet = Tweet.new(tweet_params)
+   def create
+     @tweet = Tweet.create(tweet_params)
+     if @tweet.save
+       redirect_to tweets_path,notice:"投稿しました！"
      else
-       @tweet = Tweet.new
+       render 'new'
      end
-  end
+   end
 
-  def create
-    @tweet = Tweet.create(tweet_params)
-    if @tweet.save
-      redirect_to tweets_path,notice:"投稿しました！"
-    else
-      render 'new'
-    end
-  end
+   def show
+   end
 
-  def show
-  end
+   def edit
+   end
 
-  def edit
-  end
+   def update
+      if @tweet.update(tweet_params)
+        redirect_to tweets_path,notice:"投稿内容を変更しました！"
+     else
+       render 'edit'
+     end
+   end
 
-  def update
-     if @tweet.update(tweet_params)
-       redirect_to tweets_path,notice:"投稿内容を変更しました！"
-    else
-      render 'edit'
-    end
-  end
+   def destroy
+     @tweet.destroy
+     redirect_to tweets_path,notice:"投稿を削除しました！"
+   end
 
-  def destroy
-    @tweet.destroy
-    redirect_to tweets_path,notice:"投稿を削除しました！"
-  end
+   def confirm
+     @tweet = Tweet.new(tweet_params)
+     render :new if @tweet.invalid?
+   end
 
-  def confirm
-    @tweet = Tweet.new(tweet_params)
-    render :new if @tweet.invalid?
-  end
+   private
 
-private
+   def tweet_params
+       params.require(:tweet).permit(:content)
+   end
 
-  def tweet_params
-    params.require(:tweet).permit(:content)
-  end
-
-  def set_tweet
-    @tweet = Tweet.find(params[:id])
-  end
-
+   def set_tweet
+       @tweet = Tweet.find(params[:id])
+   end
 end
